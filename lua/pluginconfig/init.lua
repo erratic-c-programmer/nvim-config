a6bd218d851053689108d41b72c2d0125f2bc339
Isaac <tux314159@gmail.com>
2022-05-15 10:50:26 +0800

    Sun May 15 10:50:26 AM +08 2022

--- lua/pluginconfig/init.lua
+++ lua/pluginconfig/init.lua
@@ -1,7 +1,7 @@
 local paths = vim.split(vim.fn.glob("~/.config/nvim/lua/pluginconfig/*"), "\n")
 
 for i, file in pairs(paths) do
-  if not string.match(file, ".*init\.lua$" then
+  if not string.match(file, ".*init\\.lua$") then
     vim.cmd("source " .. file)
   end
 end
