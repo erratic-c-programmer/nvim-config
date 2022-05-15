d7532f4519124ee1850e07fd778b8c9946921e92
Isaac <tux314159@gmail.com>
2022-05-15 10:47:04 +0800

    Sun May 15 10:47:04 AM +08 2022

--- lua/pluginconfig/init.lua
+++ lua/pluginconfig/init.lua
@@ -1,4 +1,4 @@
-local paths = vim.split(vim.fn.glob("~/.config/nvim/lua/pluginconfig*"), "\n")
+local paths = vim.split(vim.fn.glob("~/.config/nvim/lua/pluginconfig/*"), "\n")
 
 for i, file in pairs(paths) do
   if file ~= "init.lua"
