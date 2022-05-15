b0a3a84df2ed17b94e6322e018ae142da1498c8a
Isaac <tux314159@gmail.com>
2022-05-15 11:12:21 +0800

    Sun May 15 11:12:21 AM +08 2022

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,10 +2,15 @@ return require('packer').startup(function()
   use "EdenEast/nightfox.nvim"
   use {
     "feline-nvim/feline.nvim",
-    requires={ "kyazdani42/nvim-web-devicons" }
+    requires={ "kyazdani42/nvim-web-devicons" },
     config=function() require('feline').setup() end
   }
-  use "lewis6991/gitsigns.nvim"
+  use {
+    "lewis6991/gitsigns.nvim"
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
   use "LionC/nest.nvim"
   use { "ms-jpq/coq_nvim", branch="coq" }
   use "neovim/nvim-lspconfig"
