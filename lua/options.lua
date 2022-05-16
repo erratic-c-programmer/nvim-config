641a859455997d8c83ea153f8b0cac0f4f3bc7a5
Isaac <tux314159@gmail.com>
2022-05-16 22:22:54 +0800

    Mon May 16 10:22:54 PM +08 2022

--- lua/options.lua
+++ lua/options.lua
@@ -30,9 +30,10 @@ u.ft_autocmd_("lua", u.au_indent(2))
 vim.cmd("autocmd BufWritePost ~/.config/nvim/* :silent exec '! (cd ~/.config/nvim/; git add .; git commit -m \"$(date)\"; git push)'")
 
 -- wrap only at words for text files, long-line handling...
+local wrapmapstr = 
 vim.cmd("autocmd BufEnter * if empty(&filetype) | :set linebreak | endif")
 vim.cmd("autocmd FileType text,html,tex :set linebreak")
 vim.cmd("autocmd FileType text,html,tex :nnoremap j gj| :nnoremap k gk| :nnoremap 0 g0| :nnoremap $ g$| :nnoremap ^ g^")
-vim.cmd("autocmd BufEnter * if empty(&filetype) | :nnoremap j gj| :nnoremap k gk| :nnoremap 0 g0| :nnoremap $ g$| :nnoremap ^ g^| endif")
+vim.cmd("autocmd BufEnter * if empty(&filetype) | nnoremap j gj| nnoremap k gk| nnoremap 0 g0| nnoremap $ g$| nnoremap ^ g^| nnoremap A g$a| endif")
 
 vim.g.coq_settings = { display = { pum = { fast_close = false }}}
