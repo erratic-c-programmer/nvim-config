fc7d5c029491d1b1230482f8f394ab82eb4f4032
Isaac <tux314159@gmail.com>
2022-05-31 22:49:13 +0800

    Tue 31 May 2022 10:49:13 PM +08

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,10 +5,10 @@ return require('packer').startup(function()
     requires={ "kyazdani42/nvim-web-devicons" },
     config=function() require('feline').setup() end
   }
+  use "itchyny/vim-haskell-indent"
   use "LionC/nest.nvim"
   use { "ms-jpq/coq_nvim", branch="coq", requires={ "ms-jpq/coq.artifacts" }, run={ ":COQdeps" } }
   use "neovim/nvim-lspconfig"
-  use "neovimhaskell/haskell-vim"
   use { "nvim-treesitter/nvim-treesitter", run=":TSUpdate" }
   use { "p00f/nvim-ts-rainbow" }
   use "sainnhe/everforest"
