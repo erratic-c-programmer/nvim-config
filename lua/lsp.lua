f40a38ddc667a38d5eae1930366dfa03a3664994
Isaac <tux314159@gmail.com>
2022-06-10 13:50:35 +0800

    Fri Jun 10 01:50:35 PM +08 2022

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,7 +1,8 @@
+nvim_lsp = require("lspconfig")
 local coq = require "coq"
 local servers = { "clangd", "pyright", "tsserver", "rust_analyzer", "hls", "csharp_ls" }
 for k, v in pairs(servers) do
-	require('lspconfig')[v].setup(
+	nvim_lsp[v].setup(
 		coq.lsp_ensure_capabilities({
 			on_attach = on_attach,
 			flags = {
@@ -10,6 +11,16 @@ for k, v in pairs(servers) do
 		})
 	)
 end
+
+nvim_lsp.rls.setup {
+  settings = {
+    rust = {
+      unstable_features = true,
+      build_on_save = false,
+      all_features = true,
+    },
+  },
+}
 
 vim.diagnostic.config({
   virtual_text = false,
